c374080b9066e554f6792d0591502414a994ed06
DdCSxd <125690368+DdCSxd@users.noreply.github.com>
2025-01-02 18:41:33 -0300

    Update Desafio.java

--- src/Desafio.java
+++ src/Desafio.java
@@ -11,7 +11,7 @@ public class Desafio {
         System.out.println("\nNome do cliente: " + nome);
         System.out.println("Tipo conta: " + tipoConta);
         System.out.println("Saldo atual: " + saldo);
-        System.out.println("\n***********************");
+        System.out.println("\n*********************");
 
         String menu = """
                 ** Digite sua opção **
